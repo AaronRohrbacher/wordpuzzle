6dfa696d02a791db7cf8aaabae02f752afd81dda
Aaron Rohrbacher and David Hale <david@>
2017-08-15 09:45:59 -0700

    add user input to create puzzle

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
-var puzzle = "Believe you can and you're halfway there. Theodore Roosevelt";
-
-var vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"];
-
-puzzle = puzzle.split('');
+$(document).ready(function() {
+  $("form#puzzle").submit(function(event) {
 
 
-for (var i = 0; i < vowels.length; i++) {
-  for (var j = 0; j < puzzle.length; j++) {
-    if (vowels[i] == puzzle[j]) {
-      puzzle[j] = '-';
+    var puzzle = $("#string").val();
+
+    var vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"];
+
+    puzzle = puzzle.split('');
+
+
+    for (var i = 0; i < vowels.length; i++) {
+      for (var j = 0; j < puzzle.length; j++) {
+        if (vowels[i] == puzzle[j]) {
+          puzzle[j] = '-';
+        }
+      }
     }
-  }
-}
-alert(puzzle.join(''));
+    alert(puzzle.join(''));
+  })
+})
